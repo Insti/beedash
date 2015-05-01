84a717d7d59c6d66f285d8345854afbb00316ce4
Geoff Hubbard <ghub@instihost.com>
2015-05-01 09:24:20 +0200

    Use the api token from an environment variable.
    Also updated Readme.

--- secrets.py
+++ secrets.py
@@ -1,4 +1,12 @@
 # Your auth token from https://www.beeminder.com/settings/advanced_settings
 # Don't share this with people unless you want them to be able to control
 # your account!
+
 BEEMINDER_AUTH_TOKEN = ''
+
+# If you didn't set it above, load it from an environment variable.
+if BEEMINDER_AUTH_TOKEN == '':
+    import os
+    BEEMINDER_AUTH_TOKEN = os.environ['BEEMINDER_AUTH_TOKEN']
+
+
